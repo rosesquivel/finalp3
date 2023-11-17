b85e6c81a67ab9777f29c4390b07a628b76d3d08
franciscomanoukian <fmanoukian@udesa.edu.ar>
2023-11-17 10:12:18 -0300

    Ahora si listo comments

--- finalp3/src/screens/Comments/Comments.js
+++ finalp3/src/screens/Comments/Comments.js
@@ -9,45 +9,63 @@ class Comments extends Component {
         super(props)
         this.state={
             textoComment: '',
-            comentariosExistentes: []
+            comentarios: undefined,
+            todosPosts: []
         }
     }
 
     componentDidMount(){
-        this.setState({
-            comentariosExistentes: this.props.route.params.infoPost.datos.comments
-        })
+        //Traer datos
+        db.collection('posts')
+        .onSnapshot(
+            posteos => {
+                let postsAMostrar = [];
+
+                posteos.forEach( unPost => {
+                    if(unPost.id == this.props.route.params.infoPost.id){
+                    console.log('ID Coincide');
+                        postsAMostrar.push(
+                        {
+                            postFiltrado: unPost.data()
+                        }
+                    )}
+                })
+
+                this.setState({
+                    comentarios: postsAMostrar[0].postFiltrado.comments
+                })
+            }
+        )
     }
 
     guardarComment() {
         db.collection('posts').doc(this.props.route.params.infoPost.id).update({
-            comments: firebase.firestore.FieldValue.arrayUnion({ text: this.state.textoComment, userEmail: auth.currentUser.email })
+            comments: firebase.firestore.FieldValue.arrayUnion({ text: this.state.textoComment, userEmail: auth.currentUser.email, createdAt: Date.now() })
         })
             .then(res => {
-                //this.comentariosExistentes.push(text)
+                //this.state.comentarios.push({ text: this.state.textoComment, userEmail: auth.currentUser.email, createdAt: Date.now() })
                 this.setState({
                     textoComment: '',
                 })
-                this.props.navigation.navigate('Home')
             })
             .catch(e => console.log(e))
     }
 
     render(){
-        console.log(this.props.route.params.infoPost);
-        console.log(this.state.comentariosExistentes);
+        console.log('En render de comments:');
+        console.log(this.state.comentarios);
         return(
             <ScrollView>
-                <TouchableOpacity style={styles.button} onPress={()=>this.props.navigation.navigate('Home')}>
+                <TouchableOpacity style={styles.buttonBack} onPress={()=>this.props.navigation.navigate('Home')}>
                     <Text style={styles.textButton}>Back</Text>{/* ver si existe styles Text button, lo saque de profile */}
                 </TouchableOpacity>
                 <Text style={styles.postOwner}>{this.props.route.params.infoPost.datos.owner}'s post:</Text>
                 <Text style={styles.postText}>{this.props.route.params.infoPost.datos.textoPost}</Text>
                 <Text style={styles.screenTitle}>Comments</Text>
-                {(this.props.route.params.infoPost.datos.comments && this.props.route.params.infoPost.datos.comments.length > 0 ? null : <Text style={styles.noComments}>Be the first to comment!</Text>)}
-                {this.props.route.params.infoPost.datos.comments && this.props.route.params.infoPost.datos.comments.length > 0 ?
+                {(this.state.comentarios && this.state.comentarios.length > 0 ? null : <Text style={styles.noComments}>Be the first to comment!</Text>)}
+                {this.state.comentarios && this.state.comentarios.length > 0 ?
                     <FlatList
-                        data={this.props.route.params.infoPost.datos.comments}
+                        data={this.state.comentarios}
                         keyExtractor={key => key.text + key.user}
                         renderItem={(comment) => <View  style={styles.unPostContainer}><TouchableOpacity onPress={() => this.props.navigation.navigate('OtherProfile', { userData: comment.item.userEmail, navigation: this.props.navigation })}>
                         <Text style={styles.commenterEmail}>{comment.item.userEmail}:</Text>
@@ -62,7 +80,7 @@ class Comments extends Component {
                         keyboardType='default'
                         value={this.state.textoComment}
                     />
-                    {this.state.textoComment === "" ? null : <TouchableOpacity style={styles.button} onPress={() => this.guardarComment()}>
+                    {this.state.textoComment === "" ? null : <TouchableOpacity style={styles.buttonComment} onPress={() => this.guardarComment()}>
                         <Text style={styles.textButton} >Add</Text>
                     </TouchableOpacity>}
                 </View>
@@ -123,7 +141,18 @@ const styles = StyleSheet.create({
         borderRadius: 6,
 
     },
-    button: {
+    buttonComment: {
+        height: 25,
+        width: "15%",
+        backgroundColor: '#46627f',
+        marginLeft: "5%",
+        textAlign: 'center',
+        borderRadius: 4,
+        borderWidth: 1,
+        borderStyle: 'solid',
+        borderColor: '#46627f',
+    },
+    buttonBack: {
         height: 25,
         width: "15%",
         backgroundColor: '#46627f',
